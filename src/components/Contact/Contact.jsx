01f10120482128fdcc3f36da91ca4cd267cb9ca8
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-17 10:04:36 +0200

    whatsapp contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -22,25 +22,38 @@ const Contact = () => {
                         <Col xs="12" lg="12" className="contact-title text-center">
                             <h1>Contacto</h1>
                             <h2>
-                                <img
-                                    src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/ynpdtsaqljmrrf3iuuwh"
-                                    alt="Phone Icon"
-                                    className="contact-icon"
-                                />
+                                <a href='https://api.whatsapp.com/send?phone=618069336'>
+                                    <img
+                                        src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/ynpdtsaqljmrrf3iuuwh"
+                                        alt="Phone Icon"
+                                        className="contact-phone-icon"
+                                    />
+                                </a>
                                 <a href="tel:+34618069336">618 069 336</a>
                             </h2>
                             <h2>
-                                <img
-                                    src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/sfqupboic0b9xzboktab"
-                                    alt="Email Icon"
-                                    className="contact-icon"
-                                />
+                                <a href='https://api.whatsapp.com/send?phone=618069336'> <img
+                                    src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/er26xxeyhz5bysgkvpgw"
+                                    alt="Whatsapp Icon"
+                                    className="contact-whatsapp-icon"
+                                /></a>
+                                <a href="https://api.whatsapp.com/send?phone=618069336">WhatsApp</a>
+                            </h2>
+
+                            <h2>
+                                <a href='mailto:info@instrusteril.es'>
+                                    <img
+                                        src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/sfqupboic0b9xzboktab"
+                                        alt="Email Icon"
+                                        className="contact-email-icon"
+                                    />
+                                </a>
                                 <a href="mailto:info@instrusteril.es">info@instrusteril.es</a>
                             </h2>
 
                             <div>
                                 <button onClick={openModal} className="btn mt-4" aria-label="Contactar ahora">
-                                    <span role="img" aria-label="Email">✉️</span> Contactar ahora
+                                    <span role="img" aria-label="Email"><img src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/gx9au2t9mv0vxigps5np" alt="Contact now icon" /></span> Contactar ahora
                                 </button>
                                 <Modal centered show={showModal} onHide={closeModal} size="lg">
                                     <Modal.Header closeButton />
