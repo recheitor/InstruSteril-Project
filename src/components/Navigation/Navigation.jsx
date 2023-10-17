4cb18a5e5f56412703c112e91e8e73aa2789d754
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-17 16:46:04 +0200

    test

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,35 @@
 import { Col, Row, } from 'react-bootstrap'
 import './Navigation.css'
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
 const Navigation = () => {
+
+
+
+    const [scrolling, setScrolling] = useState(false);
+
+    useEffect(() => {
+        // Function to handle the scroll event
+        function handleScroll() {
+            if (window.scrollY > 100) {
+                setScrolling(true);
+            } else {
+                setScrolling(false);
+            }
+        }
+
+        // Add the scroll event listener when the component mounts
+        window.addEventListener('scroll', handleScroll);
+
+        // Remove the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
+
+
+
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -12,75 +39,89 @@ const Navigation = () => {
 
     return (
         <>
-            <Row className="navi justify-content-between align-items-center" role="navigation" aria-label="Main Navigation">
-                <Col xs="2" lg="3" className="text-start">
-                    <div className="nav-head">
-                        <a href="#">
-                            <img
+
+            <div>
+                <div >
+                    <Row className={`navi justify-content-between align-items-center ${scrolling ? 'white-background' : ''}`} role="navigation" aria-label="Main Navigation">
+                        <Col xs="2" lg="3" className="text-start">
+                            <div className="nav-head">
+                                <a href="#">
+                                    {/* <img
                                 src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/uzu2gdul3daxcgotqi5o"
                                 alt="InstruSteril Logo"
                                 aria-label="InstruSteril"
-                            />
-                        </a>
-                    </div>
-                </Col>
-            </Row>
-            <input
-                className="menu-icon"
-                type="checkbox"
-                id="menu-icon"
-                name="menu-icon"
-                checked={isMenuOpen}
-                onChange={() => setIsMenuOpen(!isMenuOpen)}
-                role="checkbox"
-                aria-label="Menu Icon"
-            />
-            <label className="nav-label" htmlFor="menu-icon"></label>
-            <nav className="nav" role="navigation" aria-label="Mobile Navigation">
-                <ul>
-                    <li>
-                        <a
-                            href="#servicios"
-                            style={{ textDecoration: "none" }}
-                            onClick={closeMenu}
-                            role="menuitem"
-                        >
-                            SERVICIOS
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#sobre-nosotros"
-                            style={{ textDecoration: "none" }}
-                            onClick={closeMenu}
-                            role="menuitem"
-                        >
-                            QUIENES SOMOS
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#tarifas"
-                            style={{ textDecoration: "none" }}
-                            onClick={closeMenu}
-                            role="menuitem"
-                        >
-                            TARIFAS
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#contacto"
-                            style={{ textDecoration: "none" }}
-                            onClick={closeMenu}
-                            role="menuitem"
-                        >
-                            CONTACTO
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <div className="overlay" role="presentation"></div>
+                            /> */}
+
+                                    <h2 aria-label="InstruSteril">
+                                        <span>INSTRU</span>STERIL
+                                    </h2>
+                                </a>
+                            </div>
+                        </Col>
+                    </Row>
+
+
+                    <input
+                        className="menu-icon"
+                        type="checkbox"
+                        id="menu-icon"
+                        name="menu-icon"
+                        checked={isMenuOpen}
+                        onChange={() => setIsMenuOpen(!isMenuOpen)}
+                        role="checkbox"
+                        aria-label="Menu Icon"
+                    />
+                    <label className="nav-label" htmlFor="menu-icon"></label>
+
+
+                    <nav className="nav" role="navigation" aria-label="Mobile Navigation">
+                        <ul>
+                            <li>
+                                <a
+                                    href="#servicios"
+                                    style={{ textDecoration: "none" }}
+                                    onClick={closeMenu}
+                                    role="menuitem"
+                                >
+                                    SERVICIOS
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#sobre-nosotros"
+                                    style={{ textDecoration: "none" }}
+                                    onClick={closeMenu}
+                                    role="menuitem"
+                                >
+                                    QUIENES SOMOS
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#tarifas"
+                                    style={{ textDecoration: "none" }}
+                                    onClick={closeMenu}
+                                    role="menuitem"
+                                >
+                                    TARIFAS
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#contacto"
+                                    style={{ textDecoration: "none" }}
+                                    onClick={closeMenu}
+                                    role="menuitem"
+                                >
+                                    CONTACTO
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="overlay" role="presentation"></div>
+                </div>
+
+            </div>
         </>
     )
 }
