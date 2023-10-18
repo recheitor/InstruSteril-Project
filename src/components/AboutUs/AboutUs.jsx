d9e2e78468b63167096e2d083c1d8c998f09e9e6
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-18 18:34:06 +0200

    about us img change

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
                     <Row className="about-us justify-content-center">
                         <Col lg="6" xs="12" className="about-us-img">
                             <img
-                                src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/jvbqwpivpnfxkim0unud"
+                                src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/oagen9poohek5baf4ber"
                                 alt="Image of our team"
                                 role="img"
                             />
