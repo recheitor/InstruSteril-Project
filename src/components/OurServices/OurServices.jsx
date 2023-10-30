2d5fc7fba6c916551b60edfbfbded8d619cc8c8e
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-30 22:02:45 +0100

    revision change

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -36,7 +36,7 @@ const OurServices = () => {
                         <div className="services-text d-flex flex-column justify-content-center align-items-center">
                             <h3 aria-label="Service Title">REVISIÓN Y REPOSICIÓN</h3>
                             <p aria-label="Service Description">
-                                Nos encargamos de la revisión del instrumental. En el proceso de limpieza manual, lubricamos las piezas móviles con aceites especiales para nuestro autoclave, así alargamos la durabilidad del instrumental. En caso de anomalías, reemplazamos la pieza dañada.
+                                Nos encargamos de la revisión del instrumental. En el proceso de limpieza manual, lubricamos las piezas móviles con aceites especiales para nuestro autoclave, así alargamos la durabilidad del instrumental. En caso de anomalías, reemplazamos la pieza dañada, bajo aceptación de presupuesto.
                             </p>
                         </div>
                     </Col>
